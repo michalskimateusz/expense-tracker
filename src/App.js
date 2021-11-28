efd4f9ee330ee9cc383343011ebc535bbbab4767
michalskimateusz <michalskimateusz@outlook.com>
2021-11-28 11:57:04 +0100

    Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -24,9 +25,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('in app.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Expense tracker</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
